864c246e8320a72f0167837efa8524337b27c9a5
Arda Gün Güneş <ardagungunes@gmail.com>
2025-05-29 21:22:47 +0300

    Game is finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,8 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +10,9 @@ public class PlayerMovement : MonoBehaviour
     InputAction jumpAction;
     Rigidbody2D playerRigidBody;
     [SerializeField] float jumpHeight = 2f;
+    bool disableInputFlag = false;
+    int score = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
     
     void Start()
     {
@@ -18,9 +24,24 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        if (jumpAction.IsPressed()) {
+        if (jumpAction.IsPressed() && !disableInputFlag)
+        {
             playerRigidBody.linearVelocity = new Vector2(playerRigidBody.linearVelocityX, jumpHeight);
         }
 
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+        Debug.Log(score);
+    }
+
+    void OnCollisionEnter2D(UnityEngine.Collision2D collision)
+    {
+        disableInputFlag = true;
+        SceneManager.LoadScene("GameOverScene");
     }
 }
